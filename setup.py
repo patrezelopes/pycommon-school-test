df68cdb1e09d08d46e7534e9233c5af120995a34
patrezelopes <patrezelopes@gmail.com>
2021-12-19 15:30:13 -0300

    included setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='pycommon-school-test',
+    version='0.0.1',
+    description='Common school test modules ',
+    url='https://github.com/patrezelopes/pycommon-school-test',
+    author='Patreze Lopes',
+    author_email='patrezelopes@gmail.com',
+    license='BSD 2-clause',
+    packages=['pycommon-school-test'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
